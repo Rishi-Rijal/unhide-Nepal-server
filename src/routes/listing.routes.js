7c2ed3b66ad2baee1f3b7defce3cef5a73ad1b14
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-14 17:43:19 +0100

    updated the routes for the application

--- src/routes/listing.routes.js
+++ src/routes/listing.routes.js
@@ -1,18 +1,26 @@
 import { Router } from "express";
-import { createListing, getListings, getListing, updateListing, deleteListing } from "../controllers/listing.controller.js";
-import {upload} from "../middlewares/multer.middleware.js";
+import { createListing, getListings, getListing, updateListing, deleteListing, getListingFiltered } from "../controllers/listing.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 const router = Router();
 
-// Example route for listings
+//route for listings
 router
-.route("/")
+    .route("/")
     .get(getListings)
     .post(upload.fields([{ name: "images", maxCount: 5 }]), createListing);
 
-router.route("/:id")
+router
+    .route("/filter")
+    .post(getListingFiltered);
+
+router
+    .route("/:id")
     .get(getListing)
     .patch(updateListing)
     .delete(deleteListing);
+
+
 
 
 export default router;

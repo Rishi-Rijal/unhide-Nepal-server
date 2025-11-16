31f82bdbfccbf7e07b35f839c50ffe6e50ffd527
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-16 10:10:48 +0100

    bug fix

--- src/db/index.db.js
+++ src/db/index.db.js
@@ -5,7 +5,7 @@ dotenv.config({ path: "./.env" });
 
 const connectDB = async () => {
     try {
-        const MONGO_URI =  process.env.ISPROD === 'true' ? process.env.MONGO_URI : process.env.MONGO_URI
+        const MONGO_URI =  process.env.ISPROD === 'true' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI
         const DB_NAME = process.env.DB_NAME;
 
         const connectionInstance = await mongoose.connect(`${MONGO_URI}/${DB_NAME}`);
